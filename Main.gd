fb33b9e78cf160f5b3fbf1afd113c38872c16ab8
ixmonke <99687465+ixmonke@users.noreply.github.com>
2022-03-07 15:45:53 -0500

    Added function for jyourth

--- Main.gd
+++ Main.gd
@@ -14,6 +14,9 @@ func _ready():
 func hello_forest():
 	print("hello forest")
 
+func hello_jyourth():
+	print("Hello jyourth!)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
